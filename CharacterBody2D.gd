4866d7676d061f22093f599f8c9d8b2907043825
TOT00O <185363860+TOT00O@users.noreply.github.com>
2025-10-08 15:03:26 +0200

    givenmoventpart2
    2ndc

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 var max_speed = 300
+var lala =20 
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction * max_speed
 	move_and_slide()
-	
+	 
