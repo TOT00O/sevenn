89223f2f9ce9c1d8aeab54164a197aac6c72d7e0
cocowow <185363637+cocowow@users.noreply.github.com>
2025-10-15 14:20:18 +0200

    faster typewriting
    yeah

--- typewriter.gd
+++ typewriter.gd
@@ -1,8 +1,8 @@
-extends Control
+extends Node
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if $RichTextLabel.visible_ratio < 1:
-		$RichTextLabel.visible_ratio += 0.1 * delta
+		$RichTextLabel.visible_ratio += 0.5 * delta
 	
