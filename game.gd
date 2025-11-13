01a946e3d2a6d85d611d55efb36bae8f210f5f9e
TOT00O <185363860+TOT00O@users.noreply.github.com>
2025-11-13 11:19:05 +0100

    sgeiuw

---
+++ game.gd
@@ -0,0 +1,18 @@
+extends Node2D  # Attach this to the root node of your game scene
+
+func _ready():
+	fade_from_black()
+
+func fade_from_black():
+	# Create a black ColorRect that covers the entire screen
+	var fade_rect = ColorRect.new()
+	fade_rect.size = get_viewport().size
+	fade_rect.color = Color.BLACK
+	fade_rect.modulate.a = 1.0  # Start fully black
+	fade_rect.z_index = 1000  # Make sure it's on top of everything
+	add_child(fade_rect)
+	
+	# Animate fade from black to transparent
+	var tween = create_tween()
+	tween.tween_property(fade_rect, "modulate:a", 0.0, 1.0)  # Fade to transparent over 1 second
+	tween.tween_callback(fade_rect.queue_free)  # Remove the fade rect when done
