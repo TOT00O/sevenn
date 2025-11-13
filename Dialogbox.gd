0e740f72a2d69a9ba1d58fcde1be1b9558c1a0e9
TOT00O <185363860+TOT00O@users.noreply.github.com>
2025-11-13 10:05:48 +0100

    dialogbox

--- Dialogbox.gd
+++ Dialogbox.gd
@@ -1,24 +1,36 @@
 extends Control
 
-
 var dialog = [
-	'text', 
-	'more text',
-	'no moew text'
+	'text lalalalallalallallalalallalalalalalallala', 
+	'more text alalapojphweohfpewhph3wphrph3h32ohr3',
+	'no more text'
 ]
 
 var dialog_index = 0
-var finsihed = false
+var finished = false
 
 func _ready():
 	load_dialog()
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
+	$"Usagi(1)".visible = finished
+	if Input.is_action_just_pressed("ui_accept") and finished:
 		load_dialog()
-	
 
 func load_dialog():
 	if dialog_index < dialog.size():
-		$MarginContainer/RichTextLabel.bbcode_text =dialog[dialog_index]
-	dialog_index +=1
+		finished = false
+		$MarginContainer/RichTextLabel.text = dialog[dialog_index]
+		$MarginContainer/RichTextLabel.visible_ratio = 0
+		
+		var tween = create_tween()
+		tween.tween_property($MarginContainer/RichTextLabel, "visible_ratio", 1, 1)
+		tween.finished.connect(_on_tween_finished)
+		
+		dialog_index += 1
+	else: 
+		queue_free()
+
+func _on_tween_finished():
+	finished = true
+
