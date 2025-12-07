d2788824281a5cbadaab0e148fff5aeab93df23f
TOT00O <185363860+TOT00O@users.noreply.github.com>
2025-12-07 15:42:15 +0100

    ATTACH

--- scripts/spritecahr.gd
+++ scripts/spritecahr.gd
@@ -4,72 +4,111 @@ var max_speed = 60
 var lala = 20 
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
+@onready var weapon_node = $Weposn  # <-- Weapon node (Node2D that contains Area2D)
 
 @export var inv: Inv
 
-# Track the last direction for idle animations
 var last_direction = Vector2.DOWN
+var attacking = false   # Track if currently attacking
+
+func _ready():
+	# Hide weapon initially
+	weapon_node.visible = false
+	
+	# Connect the signal properly
+	animation_player.animation_finished.connect(_on_animation_finished)
+
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * max_speed
-	
-	move_and_slide()
 
-	# Update animations based on movement
-	update_animation(direction)
+	# If attacking, optionally stop movement (or allow free movement)
+	if not attacking:
+		move_and_slide()
+	else:
+		move_and_slide() # you can comment this line if you want movement locked while attacking
+
+	# --- Attack ---
+	if Input.is_action_just_pressed("attack") and not attacking:
+		play_attack_animation()
+
+	# Update movement animations if not attacking
+	if not attacking:
+		update_animation(direction)
+
+
+# ---------------- MOVEMENT ANIMATION LOGIC ---------------- #
 
 func update_animation(direction: Vector2):
-	# If moving, update last_direction and play walk animation
 	if direction.length() > 0:
-		# Store a normalized direction (prioritizing horizontal over vertical)
 		last_direction = get_primary_direction(direction)
 		play_walk_animation(direction)
 	else:
-		# If not moving, play idle animation based on last direction
 		play_idle_animation(last_direction)
 
 func get_primary_direction(direction: Vector2) -> Vector2:
-	# Convert diagonal movement to primary directions
 	if abs(direction.x) > abs(direction.y):
-		# Prioritize horizontal
 		return Vector2(sign(direction.x), 0)
 	else:
-		# Prioritize vertical
 		return Vector2(0, sign(direction.y))
 
+
+
 func play_walk_animation(direction: Vector2):
-	# Determine which walk animation to play based on primary direction
-	var primary_direction = get_primary_direction(direction)
-	
-	if primary_direction.x != 0:
-		# Moving horizontally
-		if primary_direction.x > 0:
+	var primary = get_primary_direction(direction)
+
+	if primary.x != 0:
+		if primary.x > 0:
 			animated_sprite.play("walk_right")
 		else:
 			animated_sprite.play("walk_left")
 	else:
-		# Moving vertically
-		if primary_direction.y > 0:
+		if primary.y > 0:
 			animated_sprite.play("walk_down")
 		else:
 			animated_sprite.play("walk_up")
 
+
 func play_idle_animation(direction: Vector2):
-	# Determine which idle animation to play based on last direction
 	if direction.x != 0:
-		# Facing horizontally
 		if direction.x > 0:
 			animated_sprite.play("idle_right")
 		else:
 			animated_sprite.play("idle_left")
 	else:
-		# Facing vertically
 		if direction.y > 0:
 			animated_sprite.play("idle_down")
 		else:
 			animated_sprite.play("idle_up")
-			
-			
+
+
+# ------------------ ATTACK BOWOW ------------------ #
+
+func play_attack_animation():
+	attacking = true
+	weapon_node.visible = true  # show weapon during attack
+
+	if last_direction.x > 0:
+		animation_player.play("swing_right")
+	elif last_direction.x < 0:
+		animation_player.play("swing_left")
+	elif last_direction.y > 0:
+		animation_player.play("swing_down")
+	else:
+		animation_player.play("swing_up")
+
+
+# Called when any animation finishes
+func _on_animation_finished(anim_name):
+	# Only hide weapon if it's a swing animation
+	if anim_name.begins_with("swing"):
+		weapon_node.visible = false
+		attacking = false
+
+
+# ------------------ Inventory ------------------ #
+
 func collect(item):
 	inv.insert(item)
