32c493fe1056ab8b15786d190675b17562c95c9b
TOT00O <185363860+TOT00O@users.noreply.github.com>
2025-12-07 11:37:17 +0100

    A

--- scripts/spritecahr.gd
+++ scripts/spritecahr.gd
@@ -13,8 +13,9 @@ var last_direction = Vector2.DOWN
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * max_speed
-	move_and_slide()
 	
+	move_and_slide()
+
 	# Update animations based on movement
 	update_animation(direction)
 
