8174fc1ff03ca6f877c91e1835b7d026471d89e1
cocowow <185363637+cocowow@users.noreply.github.com>
2026-03-14 15:50:12 +0100

    keykey

--- Door.gd
+++ Door.gd
@@ -1,15 +1,10 @@
 extends StaticBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_area_2d_body_entered(body):
-	pass # Replace with function body.
+	if self.name in Global.key_found:
+		$AnimationPlayer.play("open")
+		await $AnimationPlayer.animation_finished
+		queue_free()
+	if not self.name in Global.key_found:
+		$AnimationPlayer.play("closed")
