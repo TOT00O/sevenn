b85e13d31c7e123343e3783ca81a07fb09f4b38d
TOT00O <185363860+TOT00O@users.noreply.github.com>
2026-03-17 13:21:09 +0100

    aa

---
+++ timer.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var timer = $Timer
+@onready var label = $Label
+
+var time_left = 10  # 3 minutes in seconds
+
+func _ready():
+	timer.start()
+	update_label()
+
+func _process(delta):
+	if time_left > 0:
+		time_left -= delta
+		update_label()
+	else:
+		_on_timer_timeout()
+
+func update_label():
+	var minutes = int(time_left) / 60
+	var seconds = int(time_left) % 60
+	label.text = "%02d:%02d" % [minutes, seconds]
+
+
+
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/deathscreen.tscn")
